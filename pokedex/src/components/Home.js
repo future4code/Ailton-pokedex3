c27800005deff32091bed55f3922bec9f735f47f
Maria Caroline Pereira Silva <carol-silva-@hotmail.com>
2022-07-19 16:25:34 -0300

    header criado

--- pokedex/src/components/Home.js
+++ pokedex/src/components/Home.js
@@ -1,5 +1,33 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const ContainerHome = styled.div`
+  display:flex;
+  flex-direction: column;
+  margin-right: 5%;
+  margin-left: 3%;
+`
+const Headerheader = styled.div`
+    width: 100%;
+    border: 4px solid red;
+    background-color: white;
+    align-items: center;
+    justify-content: center;
+    >div{
+      position:relative;
+      >img{
+        max-width: 500px;
+      max-height: 300px;
+      position: absolute;
+
+      }
+    }
+    
+
+`
+//const PokeLogo = styled.img``
+
 
 function Home() {
   const navigate = useNavigate();
@@ -8,11 +36,23 @@ function Home() {
   };
 
   return (
-    <div>
-      <h1>Home</h1>
+    <ContainerHome>
+      <Headerheader>
+        <div>
+          <img src="https://logodownload.org/wp-content/uploads/2017/08/pokemon-logo-8.png" alt="Pokemon"/> 
+        </div>
+        <div>
+          <button>Pokedex</button>
+        </div>
+      </Headerheader>
+
+      <main>
+        <h1>Home</h1>
 
       <button onClick={GoPokedex}>Pokedex</button>
-    </div>
+      </main>
+
+    </ContainerHome>
   );
 }
 export default Home;
