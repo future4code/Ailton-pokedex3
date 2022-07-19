0684da403f04a03b77c4860ffa964342d9b5589c
Fernanda <fernanda_tiyomi@hotmail.com>
2022-07-19 16:29:18 -0300

    20 pokemons aparecendo na tela

--- pokedex/src/components/Home.js
+++ pokedex/src/components/Home.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Home() {
+  const [pokemon, setPokemon] = useState([]);
+
   const navigate = useNavigate();
   const GoPokedex = () => {
     navigate("/pokedex");
   };
 
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    await axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20")
+
+      .then((res) => {
+        console.log(res.data.results);
+        setPokemon(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
-      <h1>Home</h1>
-
+      <h1>Pokemon</h1>
       <button onClick={GoPokedex}>Pokedex</button>
+      {pokemon?.map((item) => {
+        console.log(item);
+        return (
+          <p key={item.id}>
+            {item.name} <button>capturar</button>
+          </p>
+        );
+      })}
     </div>
   );
 }
