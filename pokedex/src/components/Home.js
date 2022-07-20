9d3c0fd65ad6c78e1bf0e3928350a82f09112d21
Fernanda <fernanda_tiyomi@hotmail.com>
2022-07-19 22:19:48 -0300

    atualizando styled

--- pokedex/src/components/Home.js
+++ pokedex/src/components/Home.js
@@ -4,27 +4,55 @@ import axios from "axios";
 import styled from "styled-components";
 
 const ContainerHome = styled.div`
-  display:flex;
+  display: flex;
   flex-direction: column;
   margin-right: 5%;
   margin-left: 3%;
-`
+  background-color: #dddddd;
+  width: 1440px;
+`;
 const Headerheader = styled.div`
-    width: 100%;
-    border: 4px solid red;
-    background-color: white;
-    align-items: center;
-    justify-content: center;
-    >div{
-      position:relative;
-      >img{
-        max-width: 500px;
-      max-height: 300px;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  width: 1440px;
+  height: 160px;
+  left: 0px;
+  top: 0px;
+  > div {
+    position: relative;
+    > img {
+      max-width: 307px;
+      max-height: 113px;
       position: absolute;
-
-      }
+      margin-top: 21px;
+      margin-left: 566px;
     }
-`
+    > button {
+      background-color: #33a4f5;
+      border: 1px solid #33a4f5;
+      font-size: 20px;
+      color: white;
+      width: 150px;
+      height: 50px;
+      margin-top: 41px;
+      margin-left: 1250px;
+      border-radius: 8px;
+      padding: 4px, 10px, 4px, 10px;
+    }
+  }
+`;
+
+const Card = styled.p`
+  width: 440px;
+  height: 210px;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  margin-left: 0px;
+  margin-top: 0px;
+`;
+
 //const PokeLogo = styled.img``
 
 function Home() {
@@ -52,37 +80,32 @@ function Home() {
       });
   };
   return (
-
-    <div>
-      <h1>Pokemon</h1>
-      <button onClick={GoPokedex}>Pokedex</button>
-      {pokemon?.map((item) => {
-        console.log(item);
-        return (
-          <p key={item.id}>
-            {item.name} <button>capturar</button>
-          </p>
-        );
-      })}
-    </div>
     <ContainerHome>
       <Headerheader>
         <div>
-          <img src="https://logodownload.org/wp-content/uploads/2017/08/pokemon-logo-8.png" alt="Pokemon"/> 
+          <img
+            src="https://logodownload.org/wp-content/uploads/2017/08/pokemon-logo-8.png"
+            alt="Pokemon"
+          />
         </div>
         <div>
-          <button>Pokedex</button>
+          <button onClick={GoPokedex}>Pokedex</button>
         </div>
       </Headerheader>
 
-      <main>
-        <h1>Home</h1>
+      <div>
+        <h1>Pokemon</h1>
 
-      <button onClick={GoPokedex}>Pokedex</button>
-      </main>
-
+        {pokemon?.map((item) => {
+          console.log(item);
+          return (
+            <Card key={item.id}>
+              {item.name} <button>capturar</button>
+            </Card>
+          );
+        })}
+      </div>
     </ContainerHome>
-
   );
 }
 export default Home;
