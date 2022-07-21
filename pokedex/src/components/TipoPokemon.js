302c023ac8ece1d24e18c19b7d42a15464491b55
Fernanda <fernanda_tiyomi@hotmail.com>
2022-07-21 15:17:30 -0300

    atualizando

--- pokedex/src/components/TipoPokemon.js
+++ pokedex/src/components/TipoPokemon.js
@@ -3,9 +3,14 @@ import styled from "styled-components";
 import Poison from "./icones/Poison";
 import Grass from "./icones/Grass";
 import Fire from "./icones/Fire";
+import Fly from "./icones/Fly";
+import Bug from "./icones/Bug";
+import Water from "./icones/Water";
+import Normal from "./icones/Normal";
 
 const Card = styled.div`
   background: ${(props) => props.background || "#ad61ae"};
+
   border: 1px dashed rgba(255, 255, 255, 0.47);
   border-radius: 8px;
   padding: 5px 8px;
@@ -33,6 +38,14 @@ function TipoPokemon(props) {
         return "#70B873";
       case "fire":
         return "#F44900";
+      case "flying":
+        return "#6892B0";
+      case "bug":
+        return "#316520";
+      case "water":
+        return "#33A4F5";
+      case "normal":
+        return "#8A8A8A";
       default:
         return "#ad61ae";
     }
@@ -43,6 +56,11 @@ function TipoPokemon(props) {
       {tipo === "poison" && <Poison />}
       {tipo === "grass" && <Grass />}
       {tipo === "fire" && <Fire />}
+      {tipo === "flying" && <Fly />}
+      {tipo === "bug" && <Bug />}
+      {tipo === "water" && <Water />}
+      {tipo === "normal" && <Normal />}
+
       <span>{tipo}</span>
     </Card>
   );
